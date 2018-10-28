003555e0aece26f23ff7b4f64927c2d4cd2901e3
Kevin K. Johnson <kevin@re-css.com>
2018-10-28 16:25:27 -0500

    min-width

--- src/App.vue
+++ src/App.vue
@@ -270,9 +270,10 @@
     display: flex;
       flex-flow: row wrap;
       justify-content: space-around;
-      align-items: flex-start;
+      align-items: stretch;
   }
   .dater_cards > div {
+    min-width: 333px;
     margin-bottom: 1em;
     padding: 1em;
     background-color: rgba(254, 186, 239, 0.5);
